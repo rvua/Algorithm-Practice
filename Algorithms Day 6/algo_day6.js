65ea31b73ec9c31c283c09e83238cb415eff241e
rvua <rvanore26@gmail.com>
2021-10-22 12:57:01 -0500

    added comments

--- Algorithms Day 6/algo_day6.js
+++ Algorithms Day 6/algo_day6.js
@@ -11,15 +11,25 @@ console.log(random);
 
 // Q.2 Dice Roller:
 // Using what we've learned about the Math library in JavaScript, complete the following function that should return a value between 1 through 6 at random.
-function d6(){
-    return Math.floor((Math.random() * 6) + 1);
+function d6() {
+    // create a variable and assign a random decimal number between 0 and 1 to it.
+    var roll = Math.random();
+    // multiply by six to change the range. now we get a decimal number between 0 and 5.
+    roll = roll * 6;
+    // add one to roll, so our range is now a decimal number from 1 to 6.
+    roll = roll + 1;
+    // chop off that decimal number with Math.floor
+    roll = Math.floor(roll);
+    // all done! return our result.
+    return roll;
 }
+  
 var playerRoll = d6();
 console.log("The player rolled: " + playerRoll);
 
-
 // Q.3 Consult the Oracle:
 // Using the following array, write a function that will answer all of our questions by randomly choosing a response
+function magic8Ball(){
 var lifesAnswers = [
     "It is certain.",
     "It is decidedly so.",
@@ -43,12 +53,14 @@ var lifesAnswers = [
     "Very doubtful."
 ];
 
-function d6(min, max) {
-    // var roll = Math.random(1,6);
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
+// let's create an index variable that's a random decimal number between 0 and the length of the lifesAnswers array.
+var index = Math.random() * lifesAnswers.length;
+// chop off that decimal on our index.
+index = Math.floor(index);
+// return a random answer
+return lifesAnswers[index];
 }
-    
-var playerRoll = d6(0,lifesAnswers.length-1);
-console.log(lifesAnswers[playerRoll]);
+
+// Hey, magic eightball, will I pass the yellow belt exam?
+var reply = magic8Ball();
+console.log(reply)
