f33e255f2ce69da5b1177b69856d97deb726b8d8
rvua <rvanore26@gmail.com>
2021-12-03 17:13:31 -0600

    finished

--- Algorithms Day 27/algorithms_day27.js
+++ Algorithms Day 27/algorithms_day27.js
@@ -17,6 +17,42 @@ var anotherSmall = [
     [43,32]
 ];
 
+function matrixSearch (larger, smaller) {
+    // Loops through the large matrix
+    for(var i = 0; i < larger.length; i++) {
+        for (var j = 0; j < larger[0].length; j++) {
+            // At every index of the larger check to see if the top left corner of the smaller matches
+            console.log("Is smaller [0][0] equal to larger [i][j]? - " + smaller[0][0]+ "," + larger[i][j]);
+
+            if (smaller[0][0] === larger[i][j]) {
+                console.log("We found a match!")
+                // If match then we continue searching for a deeper match
+                var isMatched = true;
+
+                for (var k = 0; k < smaller.length; k++) {
+                    for (var l = 0; l < smaller[0].length; l++) {
+                        // Checks to see if there are no matches for the bottom row in the smaller.
+                        console.log("Is smaller [k][l] not equal to larger [i+k][j+l]? - " + smaller[k][l]+ "," + larger[i+k][j+l]);
+
+                        if(smaller[k][l] != larger[i+k][j+l]) {
+                            isMatched = false;
+                            break;
+                        }
+                    }
+                    
+                    if (!isMatched) {
+                        break;
+                    }
+                }
+                if (isMatched) {
+                    return true;
+                }
+            }
+            
+        }
+    }
+    return false;
+}
 
 console.log(matrixSearch(big, small))
 console.log(matrixSearch(big, anotherSmall))
