35a72fa23839590d624e30de1ff30ab88ee0b021
rvua <rvanore26@gmail.com>
2021-12-03 10:17:37 -0600

    algo_day 27. Need to solve

---
+++ Algorithms Day 27/algorithms_day27.js
@@ -0,0 +1,22 @@
+// Matrix Search
+// Need To Solve 
+var big = [
+    [67,34,45,56],
+    [98,87,76,65],
+    [56,67,78,89],
+    [54,43,32,21]
+];
+
+var small = [
+    [67,78],
+    [43,32]
+];
+
+var anotherSmall = [
+    [67,7],
+    [43,32]
+];
+
+
+console.log(matrixSearch(big, small))
+console.log(matrixSearch(big, anotherSmall))
